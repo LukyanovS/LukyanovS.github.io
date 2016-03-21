b1b85fc381af2ba74e3d7f6e91430544f0b81e9b
Sergey <totalcomander@gmail.com>
2016-03-21 23:24:14 +0300

    21.03.home

--- final/js/app.js
+++ final/js/app.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
-    
-    $('#banner-carousel').owlCarousel({
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        animateOut: 'slideOutDown',
-        animateIn: 'slideInDown',
-        stagePadding:30,
-        smartSpeed:450,
-        items: 1,
-        navigation:true,
+    $(function(){
+        $('#Container').mixItUp();
     });
     
 });
