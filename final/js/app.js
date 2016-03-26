76eaa7a863d211632880517538eba9649d2bee60
Sergey <totalcomander@gmail.com>
2016-03-26 12:14:46 +0200

    mihail

--- final/js/app.js
+++ final/js/app.js
@@ -8,4 +8,50 @@ $(document).ready(function() {
    function(){ $(this).removeClass('rotateIn') 
     });
     
+    var topFuns = $('#funs').offset().top;
+    var flag = false;
+    
+    $(window).scroll(function(){
+        if (flag == false && $('body').scrollTop() >= topFuns - 400  ){
+            flag = true;
+                $('#one')
+                  .prop('number', 3000)
+                  .animateNumber(
+                    {
+                      number: 3200
+                    },
+                    10000
+                );    
+            
+                $('#two')
+                  .prop('number', 0)
+                  .animateNumber(
+                    {
+                      number: 120
+                    },
+                    10000
+                );
+
+                $('#three')
+                  .prop('number', 0)
+                  .animateNumber(
+                    {
+                      number: 360
+                    },
+                    10000
+                );
+
+                $('#four')
+                  .prop('number', 0)
+                  .animateNumber(
+                    {
+                      number: 42
+                    },
+                    10000
+                );
+        };
+        
+    });
+    
+
 });
