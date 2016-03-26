0c6aaaf787677977562a9b0fa90d69c1093588e7
Sergey <totalcomander@gmail.com>
2016-03-27 01:25:07 +0200

    26-27.03.home

--- final/js/app.js
+++ final/js/app.js
@@ -53,5 +53,20 @@ $(document).ready(function() {
         
     });
     
+    
+    $('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom',
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300
+		}
+	});
 
 });
