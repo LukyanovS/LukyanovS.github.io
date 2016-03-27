6b7a53819ee4e83910541ff1237fb7c3ea37a2fb
Sergey <totalcomander@gmail.com>
2016-03-27 12:00:19 +0300

    27.03 scroll to top

--- final/js/app.js
+++ final/js/app.js
@@ -68,5 +68,18 @@ $(document).ready(function() {
 			duration: 300
 		}
 	});
+    
+    $(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},1500);
+		return false;
+	});
 
 });
